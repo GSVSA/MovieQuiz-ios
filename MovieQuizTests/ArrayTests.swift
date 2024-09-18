a4b1fa003a3b613a3c35a680a976a40713a6fc49
Anna <shvets.anna.andreevna@gmail.com>
2024-09-18 15:59:34 +0300

    Tests

--- MovieQuizTests/ArrayTests.swift
+++ MovieQuizTests/ArrayTests.swift
@@ -9,7 +9,7 @@ class ArrayTests: XCTestCase {
         let value = array[safe: 2]
        // Then
         XCTAssertNotNil(value)
-        XCTAssertEqual(value, 2)
+        XCTAssertEqual(value, 3)
     }
     
     func testGetValueOutOfRange() throws { // тест на взятие элемента по неправильному индексу
